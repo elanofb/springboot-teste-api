c8c0d8d69ecf611c34905ed0b2f35e9dd54ae3df
elanofb <elanofb@gmail.com>
2020-09-21 14:09:09 -0300

    arquivos

---
+++ src/main/java/com/example/restexample/ConsumingRestApplication.java
@@ -0,0 +1,48 @@
+package com.example.restexample;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ConsumingRestApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+//	@Bean
+//	public RestTemplateCustomizer readTimeoutCustomizer() {
+//	    return restTemplate -> {
+//	        HttpComponentsClientHttpRequestFactory clientRequestFactory = new HttpComponentsClientHttpRequestFactory();
+//	        clientRequestFactory.setReadTimeout(12345);
+//	        restTemplate.setRequestFactory(clientRequestFactory);
+//	    };
+//	}
+	
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+
+
+			for (int i = 0; i < 1; i++) {
+				Quote quote = restTemplate.getForObject(						
+						"https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+				log.info("Resultado da Chamada REST: " + quote.toString());
+
+			}
+		};
+	}
+}
